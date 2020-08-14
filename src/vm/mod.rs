930d228b6aa7a00c78743a19470f9e8106da6289
Inoki <veyx.shaw@gmail.com>
2020-08-14 19:53:27 +0200

    Add init vm

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,18 +1,67 @@
-
-pub enum fct {
-    lit,
-    opr,
-    lod,
-    sto,
-    cal,
-    inte,
-    jmp,
-    jpc,
+#[derive(Copy, Clone)]
+pub enum Fct {
+    Lit,
+    Opr,
+    Lod,
+    Sto,
+    Cal,
+    Inte,
+    Jmp,
+    Jpc,
+    Hlt,    // Halt
 }
 
 /* instruction structure */
-pub struct instruction {
-    f: fct,         // instruction
-    l: i32,         // level difference between declaration and reference
-    a: i32,         // a variant depending on l
+#[derive(Copy, Clone)]
+pub struct Instruction {
+    f: Fct,         // instruction
+    l: usize,         // level difference between declaration and reference
+    a: usize,         // a variant depending on l
+}
+
+pub struct PL0VirtualMachine {
+    pc: usize,  // program counter
+    bp: usize,  // base address pointer
+
+    stack: Vec<u32>,
+
+    current_instruction: Instruction,
+    instructions: Vec<Instruction>,
+}
+
+impl PL0VirtualMachine {
+    fn load(ins: Vec<Instruction>) -> PL0VirtualMachine {
+        let vm = PL0VirtualMachine {
+            pc: 0,
+            bp: 0,
+            stack: Vec::new(),
+
+            current_instruction: Instruction {
+                f: Fct::Hlt,
+                l: 0,
+                a: 0,
+            },
+            instructions: ins,
+        };
+        vm
+    }
+
+    fn execute(&mut self) {
+        self.pc = 0;
+        self.bp = 0;
+
+        loop {
+            self.single_step_execute();     // Single step
+
+            if self.pc == 0 {
+                break;
+            }
+        }
+    }
+
+    fn single_step_execute(&mut self) {
+        self.current_instruction = self.instructions[self.pc];
+
+        // TODO: implement
+    }
 }

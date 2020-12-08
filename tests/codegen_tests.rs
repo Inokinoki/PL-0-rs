00cf19f54020d57a4f6534625f2a0d44ab84c35a
Weixuan XIAO <veyx.shaw@gmail.com>
2020-12-08 21:54:52 +0100

    Commit with tests

---
+++ tests/codegen_tests.rs
@@ -0,0 +1,22 @@
+extern crate pl0rust;
+
+#[test]
+fn test_add_into_name_table1() {
+    let mut generator = pl0rust::compile::codegen::CodeGenerator::new();
+}
+
+#[test]
+fn test_find_variable_no_duplicated_name2() {
+    let mut generator = pl0rust::compile::codegen::CodeGenerator::new();
+}
+
+#[test]
+fn test_find_variable_with_duplicated_name3() {
+    let mut generator = pl0rust::compile::codegen::CodeGenerator::new();
+}
+
+#[test]
+fn test_find_variable_not_found4() {
+    let mut generator = pl0rust::compile::codegen::CodeGenerator::new();
+
+}
---
+++ tests/factor_tests.rs
@@ -0,0 +1,3 @@
+extern crate pl0rust;
+
+
